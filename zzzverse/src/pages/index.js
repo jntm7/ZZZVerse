bd5d0272536241085c6d54074700d69f4163046d
Jonathan <108718802+jntm7@users.noreply.github.com>
2025-03-02 16:10:07 -0700

    Add global font styling

--- zzzverse/src/pages/index.js
+++ zzzverse/src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-                <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
             </Head>
             <div>
                 <main>
